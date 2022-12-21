70fb11b420871da2c7a1ec91964db0f8bd9f0855
Dang Giang <gianggiang1512@gmail.com>
2022-12-21 23:01:53 +0700

    remove unused import

--- src/main/java/com/dtg/DummyAuthorizationServer/models/User.java
+++ src/main/java/com/dtg/DummyAuthorizationServer/models/User.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.persistence.*;
 import java.util.Collection;
